1e0c71af9946a5517162ae6039adf0d4b0db10a0
Sean Gibbons <github@seangibbz.ca>
2024-11-24 19:02:47 -0800

    VirtualBox Image Conversion Batch File

---
+++ win64-convert-hdd-to-virtualbox.bat
@@ -0,0 +1,19 @@
+@echo off
+cd /d %~dp0
+echo Let's convert the Qemu Virtual HDD (.qcow2) into a VirtualBox Virtual HDD (.vdi)
+if exist "iPod.Clickwheel.Games.Preservation.Project.utm/iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2" (
+	echo Using path: iPod.Clickwheel.Games.Preservation.Project.utm/iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2
+	echo =================================================================================================
+	echo In progress... don't close this console window until it's finished. This step can take a while...
+	"win64-qemuimg/qemu-img.exe" convert -p -f qcow2 -O vdi "iPod.Clickwheel.Games.Preservation.Project.utm/iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2" "win64-virtualbox/iPod Clickwheel Games Preservation Project.vdi"
+) else (
+	if exist "iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2" (
+		echo Using path: iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2
+		echo =================================================================================================
+		echo In progress... don't close this console window until it's finished. This step can take a while...
+		"win64-qemuimg/qemu-img.exe" convert -p -f qcow2 -O vdi "iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2" "win64-virtualbox/iPod Clickwheel Games Preservation Project.vdi"
+	) else (
+		echo ERROR: Can't find "iPod Clickwheel Games Preservation Project.utm" in your current folder
+		PAUSE
+	)
+)
