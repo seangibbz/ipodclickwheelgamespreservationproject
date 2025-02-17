78f03f5a8820a3576e3ed0eee1d06e02d79831cd
gavhu10 <gavinhutten@gmail.com>
2025-02-17 12:52:51 -0500

    Make it check that win64-virtualbox directory exists

--- win64-convert-hdd-to-virtualbox.bat
+++ win64-convert-hdd-to-virtualbox.bat
@@ -1,6 +1,13 @@
 @echo off
 cd /d %~dp0
 echo Let's convert the Qemu Virtual HDD (.qcow2) into a VirtualBox Virtual HDD (.vdi)
+
+:: Check if the win64-virtualbox directory exists, and create it if it doesn't
+if not exist "win64-virtualbox" (
+    echo The directory "win64-virtualbox" does not exist. Creating it now...
+    mkdir "win64-virtualbox"
+)
+
 if exist "iPod.Clickwheel.Games.Preservation.Project.utm/iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2" (
 	echo Using path: iPod.Clickwheel.Games.Preservation.Project.utm/iPod Clickwheel Games Preservation Project.utm/Data/A973B7BF-F17A-44C5-A6D7-B6D819938FDC.qcow2
 	echo =================================================================================================
